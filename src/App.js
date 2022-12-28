d44aeca22c630a7ec55b1d7aa71f91a86068d612
Sahil pillania <sahilpillania88@gmail.com>
2022-12-28 12:09:35 +0530

    footer page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div
+      className="f"
+      // style={{
+      //   backgroundColor: darkMode ? "black" : "White",
+      //   color: darkMode && "white",
+      // }}
+    >
+      <div className="f-auth">
+        Copyright &copy; Sahil Pillania - {new Date().getFullYear()}
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
