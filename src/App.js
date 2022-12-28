52e4c6efb97671d92983e0d8f8c287eb5fdd0884
Sahil pillania <sahilpillania88@gmail.com>
2022-12-28 11:53:35 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./styles.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import "../styles.css";
+import { NavLink } from "react-router-dom";
+import logo2 from "../Images/logo2.png";
 
 const NavBar = () => {
-  return <div className="navbar"></div>;
+  const darkMode = true;
+  return (
+    <div className="N">
+      <nav
+        className={`navbar navbar-expand-lg ${
+          darkMode ? `navbar-dark bg-dark` : `navbar-light bg-light`
+        } `}
+        // style={{
+        //   backgroundColor: darkMode ? "black !important" : "white",
+        //   color: darkMode ? "white !important" : "black !important",
+        // }}
+      >
+        <div className="container-fluid mx-4">
+          <NavLink className="navbar-brand" to="/">
+            <img src={logo2} alt="logo" className="N-logo" />
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse ml-auto"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/portfolio-sahil"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link active" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link active" to="projects">
+                  Projects
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link active" to="contact">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
